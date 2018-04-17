b14a3d1babe9977581a1f7cdcf552f232ce0cc36
Marjan Ilievski <m.ilievski@datavisual.nl>
2018-04-17 16:30:58 +0200

    company component

--- frontend/src/app/company/company.component.ts
+++ frontend/src/app/company/company.component.ts
@@ -1,50 +1,50 @@
-import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
+  import {Component, OnInit} from '@angular/core';
+  import {Router} from '@angular/router';
 
-import {CompanyDTO} from '../models/company.model';
-import {CompanyService} from '../service/company.service';
-import {UserService} from '../service/user.service'   ;
+  import {CompanyDTO} from '../models/company.model';
+  import {CompanyService} from '../service/company.service';
+  import {UserService} from '../service/user.service'   ;
 
 
-@Component({
-  selector: 'app-company',
-  templateUrl: './company.component.html',
-  styleUrls: ['./company.component.css']
-})
-export class CompanyComponent implements OnInit {
+  @Component({
+    selector: 'app-company',
+    templateUrl: './company.component.html',
+    styleUrls: ['./company.component.css']
+  })
+  export class CompanyComponent implements OnInit {
 
-  companies: CompanyDTO[];
-  editMode: boolean;
-  companyForEdit: CompanyDTO;
+    companies: CompanyDTO[];
+    editMode: boolean;
+    companyForEdit: CompanyDTO;
 
-  constructor(private router: Router, private userService: UserService,
-              private companyService: CompanyService) {
-    this.editMode = false;
-  }
+    constructor(private router: Router, private userService: UserService,
+                private companyService: CompanyService) {
+      this.editMode = false;
+    }
 
-  ngOnInit() {
-    this.companyService.getCompanies()
-      .subscribe(data => {
-        this.companies = <CompanyDTO[]> data;
+    ngOnInit() {
+      this.companyService.getCompanies()
+        .subscribe(data => {
+          this.companies = <CompanyDTO[]> data;
+        });
+    }
+
+    openEditMode(company: CompanyDTO) {
+      console.log('comnany : ', company.companyName);
+      this.editMode = true;
+      this.companyForEdit = company;
+
+    }
+
+    cancelEditCompany() {
+      this.editMode = false;
+    }
+
+    saveEditedCompany() {
+      this.editMode = false;
+      this.companyService.updateCompany(this.companyForEdit).subscribe( data => {
+        alert('Company updated successfully.');
       });
-  }
-
-  openEditMode(company: CompanyDTO) {
-    console.log('comnany : ', company.companyName);
-    this.editMode = true;
-    this.companyForEdit = company;
+    }
 
   }
-
-  cancelEditCompany() {
-    this.editMode = false;
-  }
-
-  saveEditedCompany() {
-    this.editMode = false;
-    this.companyService.updateCompany(this.companyForEdit).subscribe( data => {
-      alert('Company updated successfully.');
-    });
-  }
-
-}
