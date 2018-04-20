fd71dbeab3334eb2f2681bce71d2693f4f1f0b11
Marjan Ilievski <m.ilievski@datavisual.nl>
2018-04-20 12:30:12 +0200

    filter for user implemented

--- frontend/src/app/service/user.service.ts
+++ frontend/src/app/service/user.service.ts
@@ -1,20 +1,20 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-
-import { User } from '../models/user.model';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
 };
 
 @Injectable()
 export class UserService {
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
   private userUrl = 'http://localhost:8080/api';
   private updateURL = 'http://localhost:8080/api/update';
+  private searchURL = 'http://localhost:8080/api/search';
 
   public getUsers() {
     return this.http.get(this.userUrl);
@@ -31,6 +31,17 @@ export class UserService {
   public updateCompany(company) {
     console.log('Edit company request sending ...');
     return this.http.post(this.updateURL, company);
+  }
+
+  public searchUsers(name, username, email, companyDTO) {
+    return this.http.get(this.searchURL, {
+      params: {
+        name: name,
+        username: username,
+        email: email,
+        companyDTO: companyDTO
+      }
+    });
   }
 
 }
--- frontend/src/app/user/user.component.ts
+++ frontend/src/app/user/user.component.ts
@@ -16,7 +16,10 @@ import {CompanyService} from '../service/company.service';
 export class UserComponent implements OnInit {
 
   users: User[];
+  allUsers: User[];
   companies: CompanyDTO[];
+  userSearch: User;
+  companySelected: number;
 
   constructor(private router: Router, private userService: UserService,
               private companyService: CompanyService) {
@@ -27,11 +30,16 @@ export class UserComponent implements OnInit {
     this.userService.getUsers()
       .subscribe(data => {
         this.users = <User[]> data;
+        this.allUsers = this.users;
       });
     this.companyService.getCompanies()
       .subscribe(data => {
         this.companies = <CompanyDTO[]> data;
       });
+    this.userSearch = new User();
+    this.userSearch.email = '';
+    this.userSearch.username = '';
+    this.userSearch.name = '';
   }
 
   deleteUser(user: User): void {
@@ -40,5 +48,27 @@ export class UserComponent implements OnInit {
         this.users = this.users.filter(u => u !== user);
       });
   }
+
+  startSearch(): void {
+    console.log('Start search ' + this.userSearch.name);
+    if (this.userSearch.name.length > 0 ||
+      this.userSearch.username.length > 0 ||
+      this.userSearch.email.length > 0) {
+      console.log('calling the service');
+      this.userService.searchUsers(this.userSearch.name,
+        this.userSearch.username,
+        this.userSearch.email,
+        null)
+        .subscribe(data => {
+          console.log('Data received ');
+          this.users = <User[]> data;
+          console.log('Number of users received received ' + this.users.length);
+        });
+    } else {
+      this.users = this.allUsers;
+    }
+
+  }
+
 
 }
